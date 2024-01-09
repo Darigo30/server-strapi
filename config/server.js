0f129d68a99e11bf94f8059ac990b84ff2ac0462
Dariana <dariana.gomez@uc.cl>
2024-01-09 10:28:37 -0300

    se cambia configuracion del archivo server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8000),
+  //port: env.int('PORT', 8001),
+ port: process.env.PORT,
+ //port: env('PORT', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
